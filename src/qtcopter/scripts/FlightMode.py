cdf36d25e063ca76e56a7300219600e67466ad06
Noam yogev <noamyogev84@gmail.com>
2015-04-15 17:50:34 +0300

    Added new PublishRCMessage method

--- src/qtcopter/scripts/FlightMode.py
+++ src/qtcopter/scripts/FlightMode.py
@@ -51,6 +51,17 @@ class FlightMode:
     #return value : success/failure
     def SetCurrentMode(self, mode):
         return self.__setModeService(base_mode=0, custom_mode=mode)
+    
+    #PublishRCMessage : publish new message to rc/override topic to set rc channels
+    #params : roll, pitch, throttle, yaw as integer values between 1000 to 2000
+    def PublishRCMessage(self, roll, pitch, throttle, yaw):
+        self.__rcMessage.SetRoll(roll)
+        self.__rcMessage.SetPitch(pitch)
+        self.__rcMessage.SetThrottle(throttle)
+        self.__rcMessage.SetYaw(yaw)
+        self.__rcOverrideTopic.publish(self.__rcMessage.GetRcMessage())
+        time.sleep(1) #TBD : further investigate if really needed
+
 
 
 
