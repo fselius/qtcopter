d4cfe24d8562a842cecca86ab73e713350f73c67
Efi <efi.shtain@gmail.com>
2015-04-17 01:43:43 -0700

    Added navigation folder

---
+++ src/qtcopter/src/qtcopter/navigation/CurrentState.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+import rospy
+
+class CurrentState:
+    def __init__(self):
+        self.imuTopic =
+    def Altitude(self):
+
+    def Heading(self):
+
+    def GpsPosition(self):
