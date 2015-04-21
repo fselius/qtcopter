7aa8a633e8d31ca6edbfadc5a68c81be105ee7cf
Roee <roeemz@gmail.com>
2015-04-21 14:53:16 +0300

    updated coords.py

--- src/qtcopter/src/qtcopter/missions/balldrop/coords.py
+++ src/qtcopter/src/qtcopter/missions/balldrop/coords.py
@@ -3,23 +3,23 @@
 import argparse
 import numpy as np
 
-def find_coords(distance, pixel_width, pixel_height, x_pixel, y_pixel):#, focal_length):
+def find_coords(distance, pixel_width, pixel_height, x_pixel, y_pixel, x_os, y_os):#, focal_length):
     (xx, yy) = (2.*x_pixel*distance/pixel_width, 2.*y_pixel*distance/pixel_height)
     #(x, y)   = ((distance/focal_length)*(x_pixel, y_pixel)
     #print "dx = %d dy = %d, dz = %d\n" % (x, y, distance)
-    print "dx = %f dy = %f, dz = %f\n" % (xx-s_os, yy-y_os, distance)
+    print "dx = %f dy = %f, dz = %f\n" % (xx-x_os, yy-y_os, distance)
     #return x, y
     return (xx-x_os, yy-y_os)
 
-def main();
+def main():
     parser = argparse.ArgumentParser(description='Find balldrop target location')
     parser.add_argument('--distance', '-d', default=1, type=float, help='Z axis distance to ground')
     parser.add_argument('--pixel_width', '-w', default=1, type=int, help='width of the image in pixels')
-    parser.add_argument('--pixel_height', '-h', default=1, type=int, help='height of the image in pixels')
+    parser.add_argument('--pixel_height', '-hh', default=1, type=int, help='height of the image in pixels')
     parser.add_argument('--x_pixel', '-x', default=1, type=int, help='location in image in pixels')
-    parser.add_argument('--x_pixel', '-y', default=1, type=int, help='location in image in pixels')
-    parser.add_argument('--x_offset', '-xos', default=1, type=int, help='location of objects')
-    parser.add_argument('--x_offset', '-yos', default=1, type=int, help='location of object')
+    parser.add_argument('--y_pixel', '-y', default=1, type=int, help='location in image in pixels')
+    parser.add_argument('--x_offset', '-xos', default=1, type=float, help='location of objects')
+    parser.add_argument('--y_offset', '-yos', default=1, type=float, help='location of object')
 
 
 
@@ -33,7 +33,7 @@ def main();
     x_os         = args.x_offset
     y_os         = args.y_offset
 
-    print find_coords(distance, pixel_width, pixel_height, x_pixel, y_pixel)            
+    print find_coords(distance, pixel_width, pixel_height, x_pixel, y_pixel, x_os, y_os)            
 
 if __name__=='__main__':
     main()
