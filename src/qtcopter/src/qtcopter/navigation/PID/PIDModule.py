6842ab296de79cba6ba9aeaff7fe8cf5d5decb7a
Efi <efi.shtain@gmail.com>
2015-04-22 05:50:39 -0700

    PIDController class implementation PIDNode - implements a pidmanager to manage 4 PID controllers (X,Y,Z, Theta) PIDConfig.json - holds PID coefficents and general configuration

--- src/qtcopter/src/qtcopter/navigation/PID/PIDModule.py
+++ src/qtcopter/src/qtcopter/navigation/PID/PIDModule.py
@@ -1,39 +1,58 @@
 #!/usr/bin/env python
 # Software License Agreement (BSD License)
 
-import time
-import rospy
 
-#PIDModule : A type for PID operations, all calculations logic are encapsulated
-#within this class
-class PIDModule:
+# import time
+# import rospy
 
-    def __init__(self, Kp, Kd, Ki):
-        self.__pid_sum = 0
-        self.__Kp = Kp
-        self.__Kd = Kd
-        self.__Ki = Ki
-        self.__lastTime = time.time()
 
-    def ProportionalMethod(self, delta_x):
-        return self.__Kp * delta_x
+class PIDController:
+    def __init__(self, kp, kd, ki, dt):
+        self.kp = kp
+        self.kd = kd
+        self.ki = ki
+        self.dt = dt
+        self.integral = 0
 
-    def DifferentialMethod(self, delta_x, delta_t):
-        try:
-            return self.__Kd * (delta_x / delta_t)
-        except ZeroDivisionError, e:
-            print e
-            #TODO: logger handling
+    def SetError(self, error):
+        self.error = error
 
-    def IntegralMethod(self, delta_x, delta_t):
-        print ""
-        #TODO: implement this
+    def GetFix(self):
+        self.integral += self.ki*self.error
+        return self.kp*self.error + self.integral + self.kd*self.error/self.dt
 
-    def GetPIDValue(self, delta):
-        elapsed = time.time() - self.__lastTime
-        self.__lastTime = time.time()
-        self.__pid_sum = 0
-        self.__pid_sum += self.ProportionalMethod(delta) + \
-            self.DiffrentialMethod(delta, elapsed) + self.IntegralMethod(delta, elapsed)
 
-        return self.__pid_sum
+
+# #PIDModule : A type for PID operations, all calculations logic are encapsulated
+# #within this class
+# class PIDModule:
+#
+#     def __init__(self, Kp, Kd, Ki, dt):
+#         self.__pid_sum = 0
+#         self.__Kp = Kp
+#         self.__Kd = Kd
+#         self.__Ki = Ki
+#         self.__lastTime = time.time()
+#         self.__dt = dt
+#
+#     def ProportionalMethod(self, delta_x):
+#         return self.__Kp * delta_x
+#
+#     def DifferentialMethod(self, delta_x, delta_t):
+#         try:
+#             return self.__Kd * (delta_x / delta_t)
+#         except ZeroDivisionError, e:
+#             print e
+#             #TODO: logger handling
+#
+#     def IntegralMethod(self, delta_x, delta_t):
+#         print ""
+#         #TODO: implement this
+#
+#     def GetPIDValue(self, delta):
+#         elapsed = time.time() - self.__lastTime
+#         self.__lastTime = time.time()
+#         self.__pid_sum = 0
+#         self.__pid_sum += self.ProportionalMethod(delta) + \
+#             self.DiffrentialMethod(delta, elapsed) + self.IntegralMethod(delta, elapsed)
+#         return self.__pid_sum
--- src/qtcopter/src/qtcopter/navigation/PID/PIDNode.py
+++ src/qtcopter/src/qtcopter/navigation/PID/PIDNode.py
@@ -3,65 +3,112 @@
 
 
 import rospy
-from qtcopter.msg import controller_msg, uav_msg
+from qtcopter import controller_msg, uav_msg
 from threading import Thread
 from PIDModule import PIDModule
+from PIDModule import PIDController
+from ..Configuration import Configuration
 
-#PIDNode : Type for controlling several PIDModule instances.
-#A dedicated node will be instantiated and will live during the whole run.
-#A loop with constant rate will collect the data (deltas) from the vision control topic,
-#perform all calculations on it and publish the results to the drone navigation module.
-class PIDNode:
-    pid_output = None
-    pid_input = None
+config = Configuration("PidConfig.json")
 
+class PIDManager:
     def __init__(self):
-        self.pid_input = controller_msg()
-        self.pid_output = controller_msg()
-        self.__InitWorkingThreads()
+        xConfig = config.GetConfigurationSection("X")
+        yConfig = config.GetConfigurationSection("Y")
+        zConfig = config.GetConfigurationSection("Z")
+        thetaConfig = config.GetConfigurationSection("Theta")
+        self.AxisControllers = {"X": PIDController(xConfig["KP"], xConfig["KI"], xConfig["KD"]),
+                                "Y": PIDController(yConfig["KP"], yConfig["KI"], yConfig["KD"]),
+                                "Z": PIDController(zConfig["KP"], zConfig["KI"], zConfig["KD"]),
+                                "Theta": PIDController(thetaConfig["KP"], thetaConfig["KI"], thetaConfig["KD"])
+                                }
+        rospy.Subscriber("/ip/error", controller_msg, self.DataCollector)
 
-    #InitWorkingThreads : A helper method for creating multiple threads
-    def __InitWorkingThreads(self):
-        x = Thread(target=self.__RunPIDModule1(self.pid_input.x,5,0.2,0.05))
-        x.start()
-        y = Thread(target=self.__RunPIDModule2(self.pid_input.y,5,0.2,0.05))
-        y.start()
-        z = Thread(target=self.__RunPIDModule3(self.pid_input.z,5,0.2,0.05))
-        z.start()
+    def DataCollector(self, msg):
+        self.AxisControllers["X"].SetError(msg.X)
+        self.AxisControllers["Y"].SetError(msg.Y)
+        self.AxisControllers["Z"].SetError(msg.Z)
+        self.AxisControllers["Theta"].SetError(msg.Theta)
 
-    #RunPIDModule : For each thread a PIDModule instance is created and performs continuously
-    #PID calculations
-    #Params : input - delta in meters, Kp,Kd,Ki mathematical constants
-    def __RunPIDModule1(self,input, Kp, Kd, Ki):
-        pid_module = PIDModule(Kp,Kd,Ki)
-        while(1):
-            self.pid_output.x = pid_module.GetPIDValue(input)
+    def Run(self):
+        msg = controller_msg()
+        msg.X = self.AxisControllers["X"].GetFix()
+        msg.Z = self.AxisControllers["Z"].GetFix()
+        msg.Y = self.AxisControllers["Y"].GetFix()
+        msg.Theta = self.AxisControllers["Theta"].GetFix()
+        return msg
 
-    def __RunPIDModule2(self,input, Kp, Kd, Ki):
-        pid_module = PIDModule(Kp,Kd,Ki)
-        while(1):
-            self.pid_output.y = pid_module.GetPIDValue(input)
-
-    def __RunPIDModule3(self,input, Kp, Kd, Ki):
-        pid_module = PIDModule(Kp,Kd,Ki)
-        while(1):
-            self.pid_outputs.z = pid_module.GetPIDValue(input)
 
 if __name__ == '__main__':
     try:
         rospy.init_node('pid_node', anonymous=True)
         pid_publisher = rospy.Publisher('/pid/controller_command',controller_msg,queue_size=25)
-        pidNode = PIDNode() #one instance of PIDNode
-        rate = rospy.Rate(25) #TBD
+        pidManager = PIDManager()
+        config = Configuration("PidConfig.json")
+        generalConfig = config.GetConfigurationSection("General")
+        rate = rospy.Rate(generalConfig["Rate"])
         while not rospy.is_shutdown():
-           vision_message = rospy.wait_for_message('/pid/vision_input',controller_msg) #TBD
-           pidNode.pid_input.x = vision_message.x
-           pidNode.pid_input.y = vision_message.y
-           pidNode.pid_input.z = vision_message.z
-           pid_publisher.publish(pidNode.pid_output)
-           rate.sleep()
-
-
-
+            fix = pidManager.Run()
+            rate.sleep()
+            pid_publisher.publish(fix)
     except rospy.ROSInterruptException:
         pass
+
+# #PIDNode : Type for controlling several PIDModule instances.
+# #A dedicated node will be instantiated and will live during the whole run.
+# #A loop with constant rate will collect the data (deltas) from the vision control topic,
+# #perform all calculations on it and publish the results to the drone navigation module.
+# class PIDNode:
+#     pid_output = None
+#     pid_input = None
+#
+#     def __init__(self):
+#         self.pid_input = controller_msg()
+#         self.pid_output = controller_msg()
+#         self.__InitWorkingThreads()
+#
+#     #InitWorkingThreads : A helper method for creating multiple threads
+#     def __InitWorkingThreads(self):
+#         x = Thread(target=self.__RunPIDModule1(self.pid_input.x,5,0.2,0.05))
+#         x.start()
+#         y = Thread(target=self.__RunPIDModule2(self.pid_input.y,5,0.2,0.05))
+#         y.start()
+#         z = Thread(target=self.__RunPIDModule3(self.pid_input.z,5,0.2,0.05))
+#         z.start()
+#
+#     #RunPIDModule : For each thread a PIDModule instance is created and performs continuously
+#     #PID calculations
+#     #Params : input - delta in meters, Kp,Kd,Ki mathematical constants
+#     def __RunPIDModule1(self,input, Kp, Kd, Ki):
+#         pid_module = PIDModule(Kp,Kd,Ki)
+#         while(1):
+#             self.pid_output.x = pid_module.GetPIDValue(input)
+#
+#     def __RunPIDModule2(self,input, Kp, Kd, Ki):
+#         pid_module = PIDModule(Kp,Kd,Ki)
+#         while(1):
+#             self.pid_output.y = pid_module.GetPIDValue(input)
+#
+#     def __RunPIDModule3(self,input, Kp, Kd, Ki):
+#         pid_module = PIDModule(Kp,Kd,Ki)
+#         while(1):
+#             self.pid_outputs.z = pid_module.GetPIDValue(input)
+#
+# if __name__ == '__main__':
+#     try:
+#         rospy.init_node('pid_node', anonymous=True)
+#         pid_publisher = rospy.Publisher('/pid/controller_command',controller_msg,queue_size=25)
+#         pidNode = PIDNode() #one instance of PIDNode
+#         rate = rospy.Rate(25) #TBD
+#         while not rospy.is_shutdown():
+#            vision_message = rospy.wait_for_message('/pid/vision_input',controller_msg) #TBD
+#            pidNode.pid_input.x = vision_message.x
+#            pidNode.pid_input.y = vision_message.y
+#            pidNode.pid_input.z = vision_message.z
+#            pid_publisher.publish(pidNode.pid_output)
+#            rate.sleep()
+#
+#
+#
+#     except rospy.ROSInterruptException:
+#         pass
