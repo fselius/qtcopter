1870acf2ff3e19a43aab810edd961ff8bea3ae60
Vasily <vasily@gmail.com>
2015-04-29 01:10:36 +0300

    removed extra files, added comments to coordinates convesion

--- src/qtcopter/src/qtcopter/missions/balldrop/coords.py
+++ src/qtcopter/src/qtcopter/missions/balldrop/coords.py
@@ -3,15 +3,21 @@
 import argparse
 import numpy as np
 
-def find_coords(distance, pixel_width, pixel_height, x_pixel, y_pixel, x_os, y_os):#, focal_length):
+def cam_pixel_to_xy(distance, pixel_width, pixel_height, x_pixel, y_pixel, x_os, y_os):#, focal_length):
+    '''
+    distance    - distance to ground
+    pixel_width, height - picture size in pixels
+    x_pixel, y  - pixel on image
+    x_os, y_os  - offset of camera from balldrop on quadcopter
+    '''
     (xx, yy) = (2.*x_pixel*distance/pixel_width, 2.*y_pixel*distance/pixel_height)
     #(x, y)   = ((distance/focal_length)*(x_pixel, y_pixel)
     #print "dx = %d dy = %d, dz = %d\n" % (x, y, distance)
-    print "dx = %f dy = %f, dz = %f\n" % (xx-x_os, yy-y_os, distance)
+    #print "dx = %f dy = %f, dz = %f\n" % (xx-x_os, yy-y_os, distance)
     #return x, y
     return (xx-x_os, yy-y_os)
 
-def main():
+if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Find balldrop target location')
     parser.add_argument('--distance', '-d', default=1, type=float, help='Z axis distance to ground')
     parser.add_argument('--pixel_width', '-w', default=1, type=int, help='width of the image in pixels')
@@ -33,7 +39,5 @@ def main():
     x_os         = args.x_offset
     y_os         = args.y_offset
 
-    print find_coords(distance, pixel_width, pixel_height, x_pixel, y_pixel, x_os, y_os)            
+    print cam_pixel_to_xy(distance, pixel_width, pixel_height, x_pixel, y_pixel, x_os, y_os)            
 
-if __name__=='__main__':
-    main()
