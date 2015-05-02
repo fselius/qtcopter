c687b69aba979588aae1975bbd0e78c7daa02841
Vasily <vasily@gmail.com>
2015-05-02 15:20:56 +0300

    Added odroid XU3 to find_odroid script.

--- src/qtcopter/scripts/find_odroid.py
+++ src/qtcopter/scripts/find_odroid.py
@@ -10,19 +10,22 @@ BAD:
 
 '''
 
+import sys
 import os
 from ip_scan import get_all_mac_ip
 
-MACS = [
+MACS = {
     # TODO: add more MAC addresses here
-    '36:e6:6a:0e:97:b1',
-]
+    '36:e6:6a:0e:97:b1': 'odroid XU',
+    '00:1e:06:61:7a:39': 'odroid XU3',
+}
 
 if os.geteuid() != 0:
     print 'warning: not running as root, may not work'
 
 for mac, ip in get_all_mac_ip():
-    #print '%r, %r' % (mac, ip)
-    for pattern in MACS:
+    if len(sys.argv)>1 and sys.argv[1] == '-v':
+        print '%r, %r' % (mac, ip)
+    for pattern in MACS.keys():
         if mac.startswith(pattern):
-            print mac, ip
+            print mac, ip, MACS[mac]
