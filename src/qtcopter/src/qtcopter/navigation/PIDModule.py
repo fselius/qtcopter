590431959ed41275103a5867b9d0e144313df6c7
Efi <efi.shtain@gmail.com>
2015-05-03 11:41:45 -0700

    PID control updates

--- src/qtcopter/src/qtcopter/navigation/PIDModule.py
+++ src/qtcopter/src/qtcopter/navigation/PIDModule.py
@@ -1,22 +1,76 @@
 #!/usr/bin/env python
 # Software License Agreement (BSD License)
-import rospy
-
-class PIDControl:
-    def __init__(self):
-        rospy.init_node('PIDControl', anonymous=True)
-        self.ControlOutTopic = rospy.Publisher('PID/Out')
-
-        rospy.Subscriber('/mavros/global_position/rel_alt', Float64, self.callback)
-        overridePub = rospy.Publisher('/mavros/rc/override', OverrideRCIn, queue_size=10)
-        rate = rospy.Rate(1) # 10hz
-        self.Arm()
-        self.init_channels()
 
 
+# import time
+# import rospy
 
-if __name__ == '__main__':
-    try:
-        Controllers = PIDControl()
-    except rospy.ROSInterruptException:
-        pass
+
+class PIDController:
+    def __init__(self, kp, kd, ki, dt, minLimit, maxLimit):
+        self.kp = int(kp)
+        self.kd = int(kd)
+        self.ki = int(ki)
+        self.dt = int(dt)
+        self.integral = 0
+        self.MaxLimit = int(maxLimit)
+        self.MinLimit = int(minLimit)
+        self.LastError = 0
+        self.error=0
+        print("Started PIDController with %s %s %s %s" %(kp,kd,ki,dt))
+
+    def SetError(self, error):
+        self.LastError = self.error
+        self.error = error
+        #print("Last error: %s, new error: %s" %(self.LastError, self.error))
+
+    def GetFix(self):
+        #print("Integral: %s, Diverative %s" % (self.integral, self.kd*(self.error-self.LastError)/self.dt))
+        self.integral += self.ki*self.error
+        if self.integral > self.MaxLimit:
+            self.integral = self.MaxLimit
+        elif self.integral < self.MinLimit:
+            self.integral = self.MinLimit
+        output = self.kp*self.error + self.integral + self.kd*(self.error-self.LastError)/self.dt
+        if output > self.MaxLimit:
+            output = self.MaxLimit
+        elif output < self.MinLimit:
+            output = self.MinLimit
+        print("Integral: %s, Diverative %s" % (self.integral, self.kd*(self.error-self.LastError)/self.dt))
+        return output + 1000
+
+
+
+# #PIDModule : A type for PID operations, all calculations logic are encapsulated
+# #within this class
+# class PIDModule:
+#
+#     def __init__(self, Kp, Kd, Ki, dt):
+#         self.__pid_sum = 0
+#         self.__Kp = Kp
+#         self.__Kd = Kd
+#         self.__Ki = Ki
+#         self.__lastTime = time.time()
+#         self.__dt = dt
+#
+#     def ProportionalMethod(self, delta_x):
+#         return self.__Kp * delta_x
+#
+#     def DifferentialMethod(self, delta_x, delta_t):
+#         try:
+#             return self.__Kd * (delta_x / delta_t)
+#         except ZeroDivisionError, e:
+#             print e
+#             #TODO: logger handling
+#
+#     def IntegralMethod(self, delta_x, delta_t):
+#         print ""
+#         #TODO: implement this
+#
+#     def GetPIDValue(self, delta):
+#         elapsed = time.time() - self.__lastTime
+#         self.__lastTime = time.time()
+#         self.__pid_sum = 0
+#         self.__pid_sum += self.ProportionalMethod(delta) + \
+#             self.DiffrentialMethod(delta, elapsed) + self.IntegralMethod(delta, elapsed)
+#         return self.__pid_sum
---
+++ src/qtcopter/src/qtcopter/navigation/PIDNode.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+
+
+import rospy
+from qtcopter.msg import controller_msg, uav_msg
+from PIDModule import PIDController
+import Configuration
+
+
+config = Configuration.Configuration('/home/efishtain/PycharmProjects/Quad/Repo/qtcopter/src/qtcopter/src/qtcopter/navigation/PidConfig.json')
+
+class PIDManager:
+    def __init__(self, dt, minLimit, maxLimit):
+        xConfig = config.GetConfigurationSection("X")
+        yConfig = config.GetConfigurationSection("Y")
+        zConfig = config.GetConfigurationSection("Z")
+        thetaConfig = config.GetConfigurationSection("Theta")
+        self.AxisControllers = {"X": PIDController(xConfig["KP"], xConfig["KI"], xConfig["KD"], dt, minLimit, maxLimit),
+                                "Y": PIDController(yConfig["KP"], yConfig["KI"], yConfig["KD"], dt, minLimit, maxLimit),
+                                "Z": PIDController(zConfig["KP"], zConfig["KI"], zConfig["KD"], dt, minLimit, maxLimit),
+                                "Theta": PIDController(thetaConfig["KP"], thetaConfig["KI"], thetaConfig["KD"], dt, minLimit, maxLimit)
+                                }
+        rospy.Subscriber("/ip/error", controller_msg, self.DataCollector)
+
+    def DataCollector(self, msg):
+        self.AxisControllers["X"].SetError(msg.x)
+        self.AxisControllers["Y"].SetError(msg.y)
+        self.AxisControllers["Z"].SetError(msg.z)
+        self.AxisControllers["Theta"].SetError(msg.t)
+
+    def Run(self):
+        msg = controller_msg()
+        msg.x = self.AxisControllers["X"].GetFix()
+        msg.z = self.AxisControllers["Z"].GetFix()
+        msg.y = self.AxisControllers["Y"].GetFix()
+        msg.t = self.AxisControllers["Theta"].GetFix()
+        return msg
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('pid_node')
+        pid_publisher = rospy.Publisher('/pid/controller_command',controller_msg,queue_size=1)
+        #config = Configuration("PidConfig.json")
+        generalConfig = config.GetConfigurationSection("General")
+        pidManager = PIDManager(generalConfig["Rate"], generalConfig["MinLimit"], generalConfig["MaxLimit"])
+        rate = rospy.Rate((int)(generalConfig["Rate"]))
+        while not rospy.is_shutdown():
+            fix = pidManager.Run()
+            rate.sleep()
+            pid_publisher.publish(fix)
+    except rospy.ROSInterruptException:
+        pass
+
+# #PIDNode : Type for controlling several PIDModule instances.
+# #A dedicated node will be instantiated and will live during the whole run.
+# #A loop with constant rate will collect the data (deltas) from the vision control topic,
+# #perform all calculations on it and publish the results to the drone navigation module.
+# class PIDNode:
+#     pid_output = None
+#     pid_input = None
+#
+#     def __init__(self):
+#         self.pid_input = controller_msg()
+#         self.pid_output = controller_msg()
+#         self.__InitWorkingThreads()
+#
+#     #InitWorkingThreads : A helper method for creating multiple threads
+#     def __InitWorkingThreads(self):
+#         x = Thread(target=self.__RunPIDModule1(self.pid_input.x,5,0.2,0.05))
+#         x.start()
+#         y = Thread(target=self.__RunPIDModule2(self.pid_input.y,5,0.2,0.05))
+#         y.start()
+#         z = Thread(target=self.__RunPIDModule3(self.pid_input.z,5,0.2,0.05))
+#         z.start()
+#
+#     #RunPIDModule : For each thread a PIDModule instance is created and performs continuously
+#     #PID calculations
+#     #Params : input - delta in meters, Kp,Kd,Ki mathematical constants
+#     def __RunPIDModule1(self,input, Kp, Kd, Ki):
+#         pid_module = PIDModule(Kp,Kd,Ki)
+#         while(1):
+#             self.pid_output.x = pid_module.GetPIDValue(input)
+#
+#     def __RunPIDModule2(self,input, Kp, Kd, Ki):
+#         pid_module = PIDModule(Kp,Kd,Ki)
+#         while(1):
+#             self.pid_output.y = pid_module.GetPIDValue(input)
+#
+#     def __RunPIDModule3(self,input, Kp, Kd, Ki):
+#         pid_module = PIDModule(Kp,Kd,Ki)
+#         while(1):
+#             self.pid_outputs.z = pid_module.GetPIDValue(input)
+#
+# if __name__ == '__main__':
+#     try:
+#         rospy.init_node('pid_node', anonymous=True)
+#         pid_publisher = rospy.Publisher('/pid/controller_command',controller_msg,queue_size=25)
+#         pidNode = PIDNode() #one instance of PIDNode
+#         rate = rospy.Rate(25) #TBD
+#         while not rospy.is_shutdown():
+#            vision_message = rospy.wait_for_message('/pid/vision_input',controller_msg) #TBD
+#            pidNode.pid_input.x = vision_message.x
+#            pidNode.pid_input.y = vision_message.y
+#            pidNode.pid_input.z = vision_message.z
+#            pid_publisher.publish(pidNode.pid_output)
+#            rate.sleep()
+#
+#
+#
+#     except rospy.ROSInterruptException:
+#         pass
