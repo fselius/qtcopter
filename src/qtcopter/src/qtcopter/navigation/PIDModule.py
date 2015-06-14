7f5dd82f4e289fa13db2744be8401d0f2b57866c
Efi <efi.shtain@gmail.com>
2015-06-13 17:16:51 -0700

    Added service to start/stop PID loop Added service to update gains on a specific channel A bit of refactoring

--- src/qtcopter/src/qtcopter/navigation/PIDModule.py
+++ src/qtcopter/src/qtcopter/navigation/PIDModule.py
@@ -7,7 +7,7 @@
 
 
 class PIDController:
-    def __init__(self, kp, kd, ki, dt, minLimit, maxLimit,nValue, normalizationFactor):
+    def __init__(self, kp, kd, ki, dt, minLimit, maxLimit,nValue, normalizationFactor, axis):
         self.kp = float(kp)
         self.kd = float(kd)
         self.ki = float(ki)
@@ -20,6 +20,7 @@ class PIDController:
         self.normalizationFactor = normalizationFactor
         self.nValue = nValue
         self.epsilon = 0.01
+        self.axis = axis
         print("Started PIDController with %s %s %s %s" %(kp,kd,ki,dt))
 
 
@@ -31,41 +32,40 @@ class PIDController:
         err = self.error
         lastError = self.LastError
         if abs(err)<self.epsilon:
-            return self.nValue
-        # Test it, it might be helpful, if not comment it
+            return self.nValue #maybe need to return here NO_CHANGE from RCMessageOverride constant
+
         if err > 10:
             self.integral=0
-            print "error greater than 10, maxlimit set"
+            print self.axis, ":error greater than 10, maxlimit set"
             return self.MaxLimit +self.normalizationFactor
 
         if err > 3:
             self.integral=0
-            print "fix is 1400, error >3 but <10"
+            print self.axis, ":fix is 1400, error >3 but <10"
             return 1400
 
         if err < -10:
             self.integral=0
-            print "fix is 1200, error greater than -10"
+            print self.axis, ":fix is 1200, error greater than -10"
             return 1200
 
         if err < -3:
             self.integral=0
-            print "fix is 1200, error greater than -3 but less than -10"
+            print self.axis, "error greater than -3, MinLimit set"
             return self.MinLimit+self.normalizationFactor
 
-        print "Error between -10 and 10, pid working"
-        #Comment until here if not needed
         self.integral += self.ki*err
         if self.integral > self.MaxLimit:
             self.integral = self.MaxLimit
         elif self.integral < self.MinLimit:
             self.integral = self.MinLimit
-        output = self.kp*err + self.integral + self.kd*(err-lastError)/self.dt
+        derivative = self.kd*(err-lastError)/self.dt
+        output = self.kp*err + self.integral + derivative
         if output > self.MaxLimit:
             output = self.MaxLimit
         elif output < self.MinLimit:
             output = self.MinLimit
         output = output+self.normalizationFactor
-        print("Integral: %s, Diverative %s, fix: %s" % (self.integral, self.kd*(self.error-self.LastError)/self.dt, output))
+        print self.axis, ":Error %s, Proportinal %s, Integral %s, Derivative %s, Fix %s" %(err, err*self.kp, self.integral, derivative, output)
         return output
 
