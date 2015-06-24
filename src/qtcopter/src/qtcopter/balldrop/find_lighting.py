520070a00745dccf163940b027643da47da76f2f
Vasily <vasily@gmail.com>
2015-06-24 15:29:38 +0300

    balldrop: find_lighting improvements; threshold parameter.

--- src/qtcopter/src/qtcopter/balldrop/find_lighting.py
+++ src/qtcopter/src/qtcopter/balldrop/find_lighting.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python2
 
-#from qtcopter.balldrop.polarity_find import PolarityFind
-#from qtcopter.missions.DetailedFind import DetailedFind
-
-
 import sys
 import time
 from math import ceil, floor
-sys.path.append('..')
-from DistanceFind import DistanceFind
 
 from polarity_find import PolarityFind
+#sys.path.append('..')
+#from DistanceFind import DistanceFind
+
 
 import flycapture2 as fc2
 import numpy as np
@@ -18,9 +15,10 @@ import cv2
 import cv
 
 def show_img(img):
+    while max(img.shape[:2]) > 500:
+        img = cv2.pyrDown(img)
     #ratio = 500./max(img.shape)
     #cv2.resize(img, 
-    img = cv2.pyrDown(img)
 
     cv2.imshow('model', img)
     cv2.waitKey(0)
@@ -85,24 +83,24 @@ class PTGreyCamera(object):
     def get_property_info(self, prop):
         return self.context.get_property_info(prop)
 
-def print_info(context, name, key):
-    info = context.get_property_info(key)
-    value = context.get_property(key)
-    print_data = {
-        'name': name,
-        'min': info['abs_min'],
-        'max': info['abs_max'],
-        'units': info['units'],
-        'cur': value['abs_value'],
-        'auto': value['auto_manual_mode'],
-        'abs_control': value['abs_control']
-    }
-    print '{name}: min={min:.3f} max={max:.3f} cur={cur:.3f} [{units}] auto={auto} abs_control={abs_control}'.format(**print_data)
-def print_infos(c):
-    print_info(c, 'frame rate', fc2.FRAME_RATE)
-    print_info(c, 'auto exposure', fc2.AUTO_EXPOSURE)
-    print_info(c, 'shutter', fc2.SHUTTER)
-    print_info(c, 'gain', fc2.GAIN)
+    def print_info(self, name, key):
+        info = self.get_property_info(key)
+        value = self.get_property(key)
+        print_data = {
+            'name': name,
+            'min': info['abs_min'],
+            'max': info['abs_max'],
+            'units': info['units'],
+            'cur': value['abs_value'],
+            'auto': value['auto_manual_mode'],
+            'abs_control': value['abs_control']
+        }
+        print '{name}: min={min:.3f} max={max:.3f} cur={cur:.3f} [{units}] auto={auto} abs_control={abs_control}'.format(**print_data)
+    def print_infos(self):
+        self.print_info('frame rate', fc2.FRAME_RATE)
+        self.print_info('auto exposure', fc2.AUTO_EXPOSURE)
+        self.print_info('shutter', fc2.SHUTTER)
+        self.print_info('gain', fc2.GAIN)
 
 
 
@@ -126,7 +124,7 @@ c.set_property_manual(fc2.SHUTTER, 10) # 10ms shutter (1/100, hopefully fast eno
 # if frame_rate is too high, it is set to maximum :)
 c.set_property_manual(fc2.FRAME_RATE, 100) # maximum framerate
 c.set_property_manual(fc2.GAIN, 100) # maximum gain
-print_infos(c)
+c.print_infos()
 
 c.start_capture()
 
@@ -144,27 +142,39 @@ def iter_values(context, prop, count, try_max=None):
     return np.linspace(start, stop, count)
 
 # get frames using different gains
+gains = []
 frames = []
 for gain in iter_values(c, fc2.GAIN, 20, 100):
     c.set_property_manual(fc2.GAIN, gain) # maximum framerate
     f = c.get_frame()
-    frames.append((gain, f))
+    gains.append(gain)
+    frames.append(f)
 
-finder = PolarityFind(True, 3)
+thresholds = np.linspace(0, 255, 20)
+good_gains = []
+for i, threshold in enumerate(thresholds):
+    finder = PolarityFind(True, 3, threshold=threshold)
+    line = [(finder.find_target(frame)[0] is not None) for gain, frame in zip(gains, frames)]
+    print 'threshold:', threshold, 'good:', sum(line)
+    good_gains.append(line)
+
+good_gains = np.array(good_gains, dtype=np.uint8)
+good_gains[good_gains==1] = 255
+#print 'good_gains:', good_gains
+
+#while max(good_gains.shape) < 250:
+#    good_gains = cv2.pyrUp(good_gains)
+
+ratio = 500./max(good_gains.shape)
+good_gains = cv2.resize(good_gains, (0, 0), None, ratio, ratio)
+if len(sys.argv) > 1:
+    cv2.imwrite(sys.argv[1], good_gains)
+    print 'wrote map to', sys.argv[1]
+else:
+    show_img(good_gains)
+
+'''
 # find lowest/highest gain where target is found
-# TODO: Well, this is a lie: finder is using a parameter we want to calibrate.
-# We need to take that into consideration as well.
-gains_i = [i for i, (gain, f) in enumerate(frames) if finder.find_target(f)[0] is not None]
-for i, (gain, f) in enumerate(frames):
-    if i in gains_i:
-        print gain, ':)'
-    else:
-        print gain
-
-if not gains_i:
-    print 'Could not find target :-('
-    sys.exit()
-
 high = max(gains_i)
 # TODO: I noticed that lowest gain is actually slightly higher than the next
 # one.. check if it's true
@@ -177,5 +187,4 @@ if low > 0:
 
 low, high = frames[low][0], frames[high][0]
 print 'low, high gains:', low, high
-
-
+'''
