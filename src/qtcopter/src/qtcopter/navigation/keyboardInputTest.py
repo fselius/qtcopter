3a25feddf96f4319bbb22b76570d2ac3ec40d0b7
Noam yogev <noamyogev84@gmail.com>
2015-07-23 21:46:11 +0300

    keyboard control for simulating x,y,z,t offsets.

--- src/qtcopter/src/qtcopter/navigation/keyboardInputTest.py
+++ src/qtcopter/src/qtcopter/navigation/keyboardInputTest.py
@@ -2,26 +2,18 @@
 # Software License Agreement (BSD License)
 
 import rospy
-import sys,tty,termios
 from qtcopter.msg import controller_msg
-import curses
-
-
-import sys,tty,termios
-class _Getch:
-    def __call__(self):
-            fd = sys.stdin.fileno()
-            old_settings = termios.tcgetattr(fd)
-            try:
-                tty.setraw(sys.stdin.fileno())
-                ch = sys.stdin.read(3)
-            finally:
-                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-            return ch
+from getch import getch, pause
 
 
 if __name__ == '__main__':
     try:
+        #supported keys:
+        #(a,d) -> Control ROLL
+        #(w,x) -> control PITCH
+        #(q,e) -> control THROTTLE
+        #(s) -> reset all channels (roll,pitch,throttle -> 0)
+        #(0) -> exit
 
         rospy.init_node('keyboard_control', anonymous=True)
         pub = rospy.Publisher('/pid_input',controller_msg,queue_size=10)
@@ -29,33 +21,35 @@ if __name__ == '__main__':
         msgObject = controller_msg()
         msgObject.x = msgObject.y = msgObject.z = msgObject.t = 0
         while not rospy.is_shutdown():
-            inkey = _Getch()
-            k=inkey()
-            print str(k)
-            if k=='\x1b[A':
+            k = getch.getch()
+            if k=="e":
                     print "up pressed - Throttle +"
                     msgObject.z += 0.1
-                    print "publishing to pid:"
-                    print str(msgObject)
-                    pub.publish(msgObject)
-            elif k=='\x1b[B':
+            elif k=='q':
                     print "down pressed - Throttle -"
                     msgObject.z -= 0.1
-                    print "publishing to pid:"
-                    print str(msgObject)
-                    pub.publish(msgObject)
-            elif k=='\x1b[C':
+
+            elif k=='d':
                     print "right pressed - Roll +"
-                    msgObject.x += 0.1
-                    print "publishing to pid:"
-                    print str(msgObject)
-                    pub.publish(msgObject)
-            elif k=='\x1b[D':
+                    msgObject.y += 0.1
+            elif k=='a':
                     print "left pressed - Roll -"
+                    msgObject.y -= 0.1
+            elif k=='w':
+                    print "left pressed - Pitch +"
+                    msgObject.x += 0.1
+            elif k=='x':
+                    print "left pressed - Pitch -"
                     msgObject.x -= 0.1
-                    print "publishing to pid:"
-                    print str(msgObject)
-                    pub.publish(msgObject)
+            elif k=='s':
+                    print "s pressed - Reset All"
+                    msgObject.x = msgObject.y = msgObject.z = 0
+            elif k=='0':
+                break
+
+            print "publishing to pid:"
+            print str(msgObject)
+            pub.publish(msgObject)
 
     except rospy.ROSInterruptException:
         pass
